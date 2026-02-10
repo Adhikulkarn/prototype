0c7475945e93aa32a2200b743c3e88dfd6445ba6
Aditya kulkarni <aditya.amcec23@gmail.com>
2026-02-10 13:45:53 +0530

    CROS fix

--- backend/main.py
+++ backend/main.py
@@ -36,6 +36,7 @@ app.add_middleware(
         "http://127.0.0.1:5173",
         "http://localhost:5174",
         "http://127.0.0.1:5174",
+        "https://prototype-iota-ivory.vercel.app/",
     ],
     allow_credentials=True,
     allow_methods=["*"],
