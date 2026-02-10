9320d481436d43802be621b0bf3d31bb3ab26ddb
Aditya kulkarni <aditya.amcec23@gmail.com>
2026-02-10 13:58:20 +0530

    frontend fix

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://127.0.0.1:8000";
+const BASE_URL = "https://prototype-1-xfok.onrender.com";
 
 export async function api(url, options = {}) {
   const res = await fetch(`${BASE_URL}${url}`, {
